68207b2b3cb2df8b0ad8a90b0d4f98d19f283a8a
Martin Baklid <mbaklid@gmail.com>
2015-02-23 15:31:19 +0100

    Rename test to DatabaseConnection

--- FellesProsjekt/src/temp/DatabaseConnection.java
+++ FellesProsjekt/src/temp/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Test {
+public class DatabaseConnection {
 
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException {	
 		Connection con  =  null;
