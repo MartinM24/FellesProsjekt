72c1a508b7879c6c179c64d6d699f6290836a961
Martin Baklid <mbaklid@gmail.com>
2015-02-26 11:58:17 +0100

    Add skeleton for getAllUsers method

--- src/dbconnection/DatabaseConnection.java
+++ src/dbconnection/DatabaseConnection.java
@@ -1,16 +1,21 @@
 package dbconnection;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 
+import usergroup.LoginUser;
 import usergroup.User;
 
 public class DatabaseConnection {
 	
-/*	public static void main(String[] args) {
-		User user = new User("SivFanNR1","Per", "Sandberg", "per@frp.krf", "sivErPen");
-		addUser(user);
-		System.out.println("Per er i databasen vår.");
-	} */
+//	public static void main(String[] args) {
+//		LoginUser user = new LoginUser("SivFanNR2","Trine", "Grande", "trine@venstre.hoyre", "JensErBest");
+//		addUser(user);
+//		System.out.println("User added.");
+//	}
 
 	private static Connection startCon() {	
 		Connection con  =  null;
@@ -37,23 +42,37 @@ public class DatabaseConnection {
 		      System.out.println("Epic fail: "+ex.getMessage());
 		    }
 	}
-	
-	public static void addUser(User user) {
+	/**
+	 * Adds a new loginUser to the Database
+	 * @param LoginUser user
+	 */
+	public static void addUser(LoginUser user) {
 		Connection con = startCon();
 		try {
-		String query = "insert into users (username, firstname, lastname, email)"  + "values(?, ?, ?, ?)";
+		String query = "insert into users (username, firstname, lastname, email, salt, hash)"  + "values(?, ?, ?, ?, ?, ?)";
 		PreparedStatement preparedStmt = con.prepareStatement(query);
 		preparedStmt.setString (1, user.getUsername());
 		preparedStmt.setString (2, user.getFirstname());
 		preparedStmt.setString (3, user.getLastname());
 		preparedStmt.setString (4, user.getEmail());
+		preparedStmt.setBytes(5, user.getDBSalt());
+		preparedStmt.setBytes(6, user.getDBHash());
 		preparedStmt.execute();
 		System.out.println("Everything worked");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		endCon(con);     
+	}
+	
+	/**
+	 * Gets all users in database and returns it as a List<Users>
+	 * @return List<Users>
+	 */
+
+	public static List<User> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
