1cb3e5e3b46a3370c4d18b40762a3afb0b51da37
Martin Baklid <mbaklid@gmail.com>
2015-02-26 09:43:00 +0100

    Add start og end con methods

--- src/dbconnection/DatabaseConnection.java
+++ src/dbconnection/DatabaseConnection.java
@@ -1,41 +1,50 @@
 package dbconnection;
 
 import java.sql.*;
-import java.util.Scanner;
 
 public class DatabaseConnection {
 
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException {	
+	private static Connection startCon() {	
 		Connection con  =  null;
 		try {
 		  Class.forName("com.mysql.jdbc.Driver");
 		  String url = "jdbc:mysql://mysql.stud.ntnu.no/mariuene_MMMAT";
 		  String user = "mariuene_admin";
-		  @SuppressWarnings("resource")
-		  Scanner user_input = new Scanner(System.in);
-		  System.out.println("Passord: ");
-		  String pw = user_input.next();
-		  
+		  String pw = "1234";		  
 		  con = DriverManager.getConnection(url,user,pw);
 		  System.out.println("Tilkoblingen fungerte.");
-		  
-		  Statement myStatement = con.createStatement();
-		  ResultSet myRs = myStatement.executeQuery("select * from test2");
-		  while (myRs.next()){
-			  System.out.println(myRs.getString(2)+ " motherfucking " + myRs.getString(3));
-		  }
-		  
+		 
 		  } catch (SQLException ex) {
 		    System.out.println("Tilkobling feilet: "+ex.getMessage());
 		  } catch (ClassNotFoundException ex) {
 		    System.out.println("Feilet under driverlasting: "+ex.getMessage());
-		  } finally {
-		    try {
+		  }
+		return con;
+	}
+	
+	private static void endCon(Connection con) {
+	    try {
 		      if (con !=  null) con.close();
 		    } catch (SQLException ex) {
 		      System.out.println("Epic fail: "+ex.getMessage());
 		    }
-		  }
+	}
+	
+	public static void addUser(User user) {
+		Connection con = startCon();
+/*		Her kommer det kode som bruker user
+ * 		String query = "insert into test3 (firstname, lastname, passord, epost, username)"  + "values(?, ?, ?, ?, ?)";
+		PreparedStatement preparedStmt = con.prepareStatement(query);
+		preparedStmt.setString (1, fn);
+		preparedStmt.setString (2, en);
+		preparedStmt.setString (3, pw);
+		preparedStmt.setString (4, ep);
+		preparedStmt.setString (5, bn);
+		preparedStmt.execute();
+		System.out.println("Everything worked");
+*/		
+		endCon(con);
+	      
 	}
 }
 
