34bac2559e52084d850a7a01197feeead6f39135
mariuene <marius.enerly@gmail.com>
2015-03-02 11:21:53 +0100

    Changed userconnection

--- src/dbconnection/UserConnection.java
+++ src/dbconnection/UserConnection.java
@@ -1,9 +1,12 @@
 package dbconnection;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class NewUserConnection {
+import model.LoginUser;
+
+public class UserConnection extends DatabaseConnection{
 
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException {	
 		Connection con  =  null;
@@ -52,5 +55,50 @@ public class NewUserConnection {
 		      System.out.println("Epic fail: "+ex.getMessage());
 		    }
 		  }
+	}
+
+	/**
+	 * Gets all users in database and returns it as a List<Users>
+	 * @return List<Users>
+	 */
+	
+	public static ArrayList<LoginUser> getAllUsers() {
+		Connection con = startCon();
+		ArrayList<LoginUser> userList = new ArrayList<LoginUser>();
+		try{
+			Statement myStatement = con.createStatement();
+			ResultSet myRs = myStatement.executeQuery("select * from users");
+			while (myRs.next()){
+				userList.add(new LoginUser(myRs.getString(1), myRs.getString(2), myRs.getString(3), myRs.getString(4), myRs.getBytes(5), myRs.getBytes(6)));
+			};
+			System.out.println("Everything worked");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		endCon(con);
+		return userList;
+	}
+
+	/**
+	 * Adds a new loginUser to the Database
+	 * @param LoginUser user
+	 */
+	public static void addUser(LoginUser user) {
+		Connection con = startCon();
+		try {
+		String query = "insert into users (username, firstname, lastname, email, salt, hash)"  + "values(?, ?, ?, ?, ?, ?)";
+		PreparedStatement preparedStmt = con.prepareStatement(query);
+		preparedStmt.setString (1, user.getUsername());
+		preparedStmt.setString (2, user.getFirstname());
+		preparedStmt.setString (3, user.getLastname());
+		preparedStmt.setString (4, user.getEmail());
+		preparedStmt.setBytes(5, user.getDBSalt());
+		preparedStmt.setBytes(6, user.getDBHash());
+		preparedStmt.execute();
+		System.out.println("Everything worked");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		endCon(con);     
 	}
 }
