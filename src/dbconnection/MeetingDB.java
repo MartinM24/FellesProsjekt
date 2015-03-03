409eb985262b138a0b134a6881e0d2e0c889b5e9
Martin Mølnå <martmol@stud.ntnu.no>
2015-03-03 15:38:35 +0100

    Add addMeeting in MeetingDB

--- src/dbconnection/MeetingDB.java
+++ src/dbconnection/MeetingDB.java
@@ -1,8 +1,10 @@
 package dbconnection;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,4 +46,42 @@ public class MeetingDB extends DatabaseConnection{
 		return false;
 	}	
 	
+	public static boolean addMeeting(Meeting meeting, User user){
+		//TODO skal ikke kunne legge inn i meeting uten at det blir lagt inn i participant
+		try {
+			String meetingQuery = "insert into meeting (mDescription, timeStart, timeEnd, sted)"  + "values(?, ?, ?, ?)";
+			PreparedStatement preparedMeetingStmt = con.prepareStatement(meetingQuery, 
+				    Statement.RETURN_GENERATED_KEYS);
+			preparedMeetingStmt.setString (1, meeting.getDescription());
+			preparedMeetingStmt.setTimestamp(2, meeting.getStartDB());
+			preparedMeetingStmt.setTimestamp (3, meeting.getEndDB());
+			preparedMeetingStmt.setString(4, meeting.getPlace());
+			int res = preparedMeetingStmt.executeUpdate();
+			if (res > 0) {
+				System.out.println("Inserting Meeting worked");
+			}
+			
+			ResultSet tableKeys = preparedMeetingStmt.getGeneratedKeys();
+			String participantQuery = "insert into participant (meetingID,username)" + "values(?, ?)";
+			PreparedStatement preparedParticipantStmt = con.prepareStatement(participantQuery);
+			tableKeys.next();
+			preparedParticipantStmt.setInt(1, tableKeys.getInt(1));
+			preparedParticipantStmt.setString(2, user.getUsername());
+			int res2 = preparedParticipantStmt.executeUpdate();
+			if(res2 > 0){
+				System.out.println("Inserting Participant worked");
+				return true;
+			}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return false; 
+	}
+	
+	public static void main(String[] args) {
+		DatabaseConnection.startCon();
+		User user = UserDB.getUser("mart");
+		Meeting meeting = new Meeting(user, "a place", LocalDateTime.now(), LocalDateTime.now(), "desc", new ArrayList<User>());
+		MeetingDB.addMeeting(meeting, user);
+	}
 }
