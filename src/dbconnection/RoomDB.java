34a3305099ab95a2c252e209d7c7f850b3c45c3e
Martin Baklid <mbaklid@gmail.com>
2015-03-06 14:09:05 +0100

    Add RoomDB and Room logic

---
+++ src/dbconnection/RoomDB.java
@@ -0,0 +1,70 @@
+package dbconnection;
+
+import model.Room;
+import model.User;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Martin on 06.03.15.
+ */
+public class RoomDB extends DatabaseConnection {
+
+    private RoomDB() {
+    }
+
+    public static int addRoom(Room room){
+        //TODO skal ikke kunne legge inn i meeting uten at det blir lagt inn i participant
+        try {
+            String meetingQuery = "insert into room (roomName, capacity)"  + "values(?, ?)";
+            PreparedStatement preparedMeetingStmt = con.prepareStatement(meetingQuery,
+                    Statement.RETURN_GENERATED_KEYS);
+            preparedMeetingStmt.setString(1, room.getName());
+            preparedMeetingStmt.setInt(2, room.getCapacity());
+            int res = preparedMeetingStmt.executeUpdate();
+            if (res > 0) {
+                System.out.println("Inserting room worked");
+            }
+            ResultSet tableKeys = preparedMeetingStmt.getGeneratedKeys();
+            tableKeys.next();
+            return tableKeys.getInt(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public static List<Room> getAllRooms(User user){
+        List<Room> roomList = new ArrayList<>();
+        try{
+            Statement myStatement = con.createStatement();
+            ResultSet myRs = myStatement.executeQuery("SELECT room.roomID FROM room");
+            while (myRs.next()){
+                roomList.add(getRoom(myRs.getInt(1)));
+            }
+            System.out.println("Everything worked");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return roomList;
+    }
+
+    public static Room getRoom(int roomID){
+        try{
+            Statement sqlSelect = con.createStatement();
+            ResultSet myRs = sqlSelect.executeQuery("select * from room WHERE room.meetingID = '"+roomID+"'");
+            myRs.next();
+            return new Room(myRs.getInt(1), myRs.getString(2), myRs.getInt(3));
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
--- src/model/Room.java
+++ src/model/Room.java
@@ -1,13 +1,34 @@
 package model;
 
-public class Room {
-	public String getName(){
-		//Write the get
-		return "";
-	}
+import dbconnection.RoomDB;
 
-	public int getRoomID() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+public class Room {
+
+    private final int roomID;
+    private final String name;
+    private final int capacity;
+
+    public Room(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        this.roomID = RoomDB.addRoom(this);
+    }
+
+    public Room(int roomID, String name, int capacity) {
+        this.roomID = roomID;
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
 }
