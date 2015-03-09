e60d8dac87ee6a5b7a2045bc33f5883d64c58e72
Anna Berg <annam.berg93@gmail.com>
2015-03-09 13:48:12 +0100

    Fixed spelling error

---
+++ src/gui/ChoosenCalendarsController.java
@@ -0,0 +1,52 @@
+package gui;
+
+import calendarClient.CalendarClient;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import calendarClient.CalendarClient;
+
+/**
+ * Created by Anna on 09.03.15.
+ */
+
+public class ChoosenCalendarsController implements ControlledScreen, Initializable {
+	MainController myController;
+	
+	@FXML Button okButton;
+	@FXML Button cancelButton;
+//	@FXML Button saveButton;
+	
+	@FXML
+    public void okButtonClick(ActionEvent e){
+		//Move user back to calendar. 
+		myController.setView(CalendarClient.CALENDAR_VIEW);
+	}
+    
+    @FXML
+    public void cancelButtonClick(ActionEvent e){
+		//Move user back to calendar. 
+		myController.setView(CalendarClient.CALENDAR_VIEW);
+	}
+    
+
+	@Override
+	public void setScreenParent(MainController screenPage) {
+		this.myController = screenPage;
+		
+	}
+	
+	@Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
