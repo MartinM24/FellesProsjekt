5d2268fe6e314fe1515f98eb675dbbcccd94bc2e
mariuene <marius.enerly@gmail.com>
2015-03-10 13:45:01 +0100

    Small changes to meetingroomOverveiw.

--- src/dbconnection/MeetingDB.java
+++ src/dbconnection/MeetingDB.java
@@ -23,7 +23,7 @@ public class MeetingDB extends DatabaseConnection{
 		
 	}
 	
-	/*public static List<Meeting> getAllMeetings(User user){
+	public static List<Meeting> getAllMeetings(User user){
 		List<Meeting> meetingList = new ArrayList<Meeting>();
 		try{
 			Statement myStatement = con.createStatement();
@@ -36,7 +36,7 @@ public class MeetingDB extends DatabaseConnection{
 			e.printStackTrace();
 		}
 		return meetingList;
-	}*/
+	}
 	
 	public static Meeting getMeeting(int meetingID){
 		try{
--- src/gui/MeetingRoomOverveiwController.java
+++ src/gui/MeetingRoomOverveiwController.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import calendarClient.CalendarClient;
 import dbconnection.MeetingDB;
 import dbconnection.RoomDB;
 import model.Meeting;
@@ -14,6 +15,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -26,6 +28,7 @@ public class MeetingRoomOverveiwController implements ControlledScreen, Initiali
 	@FXML TableColumn<Room, String> nameColumn;
 	@FXML TableColumn<Room, String> capacityColumn;
 	@FXML TableColumn<Room, String> statusColumn;
+	@FXML Label warning;
 	@FXML Button chooseButton;
 	
 	public MeetingRoomOverveiwController(Meeting meeting){
@@ -35,24 +38,26 @@ public class MeetingRoomOverveiwController implements ControlledScreen, Initiali
 	public void initialize(URL location, ResourceBundle resources) {	
 		nameColumn.setCellValueFactory(new PropertyValueFactory<Room, String>("Navn"));   		
 		capacityColumn.setCellValueFactory(new PropertyValueFactory<Room, String>("Kapasitet"));   
-		statusColumn.setCellValueFactory(new PropertyValueFactory<Room, String>("Status")); 		
+		statusColumn.setCellValueFactory(new PropertyValueFactory<Room, String>("Status"));
 	}
 	
 	public void chooseButtonClick(ActionEvent e){
 		//TODO Get info from make meeting class. 
 		Room room = (Room)roomTable.getSelectionModel().getSelectedItem();
+		if (room.getAvalibility().equals("Ledig")) {
+			myController.setView(CalendarClient.ADD_MEETING_VIEW);			
+		} else {
+			if (warning.getText().equals("Venligst velg et ledig rom.")){
+				warning.setText("Ledig rom er rom med status ledig.");
+			}
+			warning.setText("Venligst velg et ledig rom.");
+		}
 		
 		
 	}
 	
-	
-	
-	
-	/**
-	 * Sikkelig stygg kode. Haaper den funker. 
+	/** 
 	 * Setter inn verdier i tabellen. 
-	 * @param start
-	 * @param end
 	 */
 	
 	@SuppressWarnings("unused")
@@ -67,19 +72,22 @@ public class MeetingRoomOverveiwController implements ControlledScreen, Initiali
 		} 
 		for (int i = 0; i<roomsDB.size(); i++){
 			for (int j = 0; j<availability.get(i).size(); j++){
-				if ((availability.get(i).get(j).get(0).isAfter(meeting.getTimeStart()) && availability.get(i).get(j).get(0).isBefore(meeting.getTimeEnd())) || 
-					(availability.get(i).get(j).get(1).isAfter(meeting.getTimeStart()) && availability.get(i).get(j).get(1).isBefore(meeting.getTimeEnd())) ||
-					(availability.get(i).get(j).get(0).isBefore(meeting.getTimeStart()) && availability.get(i).get(j).get(1).isAfter(meeting.getTimeEnd())) ||
-					(availability.get(i).get(j).get(0).isEqual(meeting.getTimeStart()) || availability.get(i).get(j).get(1).isEqual(meeting.getTimeEnd())) 
-					){
+				if (checkAvailability(availability.get(i).get(j))){
 					status.set(i, "Opptatt");
 				}
 			}
-			rooms.add(new Room(roomsDB.get(i).getName(),  Integer.toString(roomsDB.get(i).getCapacity()), status.get(i)));
+			rooms.add(new Room(roomsDB.get(i).getName(), Integer.toString(roomsDB.get(i).getCapacity()), status.get(i)));
 		}		
 		roomTable.setItems(FXCollections.observableArrayList(rooms));
 	}
 	
+	private boolean checkAvailability(ArrayList<LocalDateTime> availability) {
+		return  (availability.get(0).isAfter(meeting.getTimeStart()) && availability.get(0).isBefore(meeting.getTimeEnd())) || 
+				(availability.get(1).isAfter(meeting.getTimeStart()) && availability.get(1).isBefore(meeting.getTimeEnd())) ||
+				(availability.get(0).isBefore(meeting.getTimeStart()) && availability.get(1).isAfter(meeting.getTimeEnd())) ||
+				(availability.get(0).isEqual(meeting.getTimeStart()) || availability.get(1).isEqual(meeting.getTimeEnd()));
+	}
+
 	@Override
 	public void setScreenParent(MainController screenPage) {
 		this.myController = screenPage;
